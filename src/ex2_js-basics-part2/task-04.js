95320bbaf4a2dfc518763225a64ed81b2f806cf9
tanya <and.tati62@gmail.com>
2019-09-24 22:57:35 +0300

    добавила файл task-04.js c функцией

---
+++ src/ex2_js-basics-part2/task-04.js
@@ -0,0 +1,14 @@
+module.exports = checkReplay
+
+function checkReplay(arr) {
+	for (let i = 0; i < arr.length; i++) {
+		for (j = i + 1 ; j < arr.length; j++) {
+			if (arr[i] === arr[j]) {
+				return true;
+				break;
+			} 
+		}
+	}
+	return false;
+}
+
