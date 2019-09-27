22b6bd8f1ef331325ca6517c29a39c861378d340
tanya <and.tati62@gmail.com>
2019-09-27 14:24:17 +0300

    output line fixed

--- src/ex2_js-basics-part2/task-02.js
+++ src/ex2_js-basics-part2/task-02.js
@@ -1,16 +1,9 @@
-module.exports = showCountElem
-
 function showCountElem(arr) {
 	let count = 0;
 	for (let i = 0; i < arr.length; i++) {
-		console.log (`arr[${i}]: ${arr[i]}`);
-		count += 1;	
+	console.log ('arr[' + i + '] ' + arr[i]);
+		count += 1;
 	}
-	console.log (`Общее число элементов: ${count}`);
+	console.log ('number of items: ' + count);
 }
-
-
-
-
-
-
+module.exports = showCountElem
