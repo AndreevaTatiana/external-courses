1425dfa2d2db236d8e57bc333d343896cfc5e540
tanya <and.tati62@gmail.com>
2019-10-02 14:46:16 +0300

    deleted count

--- src/ex2_js-basics-part2/task-02.js
+++ src/ex2_js-basics-part2/task-02.js
@@ -1,9 +1,7 @@
 function showCountElem(arr) {
-	let count = 0;
 	for (let i = 0; i < arr.length; i++) {
 	console.log ('arr[' + i + '] ' + arr[i]);
-		count += 1;
 	}
-	console.log ('number of items: ' + count);
+	console.log ('number of items: ' + arr.length);
 }
 module.exports = showCountElem
