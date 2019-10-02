bf4635fd1a5af9b4e677c5af8677f4c3c83704b1
tanya <and.tati62@gmail.com>
2019-10-02 15:08:22 +0300

    changed count0 on countZero, deleted count

--- src/ex2_js-basics-part2/task-03.js
+++ src/ex2_js-basics-part2/task-03.js
@@ -1,21 +1,22 @@
 function calcEvenOdd(arr) {
-	let count0 = 0, countEven = 0, countOdd = 0, count = 0;
-	let result = [];
+	let countZero = 0;
+			countEven = 0;
+			countOdd = 0;
+			result = [];
 	
 	for (let i = 0; i < arr.length; i++){
 		if (typeof arr[i] === "number") {
 			if (arr[i] === 0) {
-				count0 += 1;
+				countZero += 1;
 			} else if (arr[i]%2 == 0) {
 				countEven +=1;
 			} else {
 				countOdd += 1;
 			}
-		} else {
-			count +=1;
 		}
 	}
-	result.push(countEven,countOdd,count0)
+	result.push(countEven,countOdd,countZero)
 	return result;
 }
+
 module.exports = calcEvenOdd
