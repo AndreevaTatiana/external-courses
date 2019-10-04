c893d02875cb92e4414ceddf2da05d022d15065d
tanya <and.tati62@gmail.com>
2019-10-04 15:37:38 +0300

    add task-09 by function insertStr

---
+++ src/ex4_js-objects-part2/task-09.js
@@ -0,0 +1,14 @@
+function insertStr(sourceStr,str,pos) {
+	let arr = sourceStr.split(' '),
+			newArr = [];
+	for (let i = 0; i < arr.length; i++) {
+		if (i === pos) {
+			 newArr.push(arr[i], str)
+		} else {
+			newArr.push(arr[i]);
+		}
+	}
+	return newArr.join(' ');
+}
+
+module.exports = insertStr
