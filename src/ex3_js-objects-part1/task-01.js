161a36c0d4f2efe8961cfe3b90243b2ab1114cf7
tanya <and.tati62@gmail.com>
2019-10-09 21:44:08 +0300

    task-01 formatting fixed

--- src/ex3_js-objects-part1/task-01.js
+++ src/ex3_js-objects-part1/task-01.js
@@ -1,7 +1,7 @@
 const user = {};
-	user.name = 'Ivan' ;
-	user.age = 25;
-	user.email = null;
-	user.isAdmin = true;
+user.name = 'Ivan';
+user.age = 25;
+user.email = null;
+user.isAdmin = true;
 
 delete user.email;
