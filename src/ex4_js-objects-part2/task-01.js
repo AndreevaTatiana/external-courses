20ba59cfec6aee80aa88bb2f2a1590cceca98870
tanya <and.tati62@gmail.com>
2019-10-09 21:50:25 +0300

    task-01 formatting fixed

--- src/ex4_js-objects-part2/task-01.js
+++ src/ex4_js-objects-part2/task-01.js
@@ -1,7 +1,6 @@
 const objProt = {a: 1, b: 2},
-			obj = Object.create(objProt);
+obj = Object.create(objProt);
 obj.c = 3;
-
 function getValue(property,obj) {
 	if (obj.hasOwnProperty(property)) {
 		return 'в прототипе нет такого свойства'
