c95b25f9ebafa19007b2d5c2eb13716b10ebe68f
tanya <and.tati62@gmail.com>
2019-10-13 20:06:13 +0300

    add task-04 by function getAnalogFilter

---
+++ src/ex6_js-array-methods/task-04.js
@@ -0,0 +1,11 @@
+function getAnalogFilter(array,callback) {
+	const newArr = [];
+	for (let i = 0; i < array.length; i++){
+		if (callback(array[i], i, array)) {
+			newArr.push(array[i]);
+		}
+	}
+	return newArr;
+}
+
+module.exports = getAnalogFilter
