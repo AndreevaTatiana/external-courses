7673824bb403b0848190efa862ecb99e4135e4b4
tanya <and.tati62@gmail.com>
2019-11-18 19:52:51 +0300

    fix task-01

--- src/ex13_js_events/task-01-2.js
+++ src/ex13_js_events/task-01-2.js
@@ -1,18 +1,11 @@
-let btnAdd = document.querySelector('.addCardBacklog');
-btnAdd.addEventListener('click', showForm);
-
-function showForm(event) {
-	document.querySelector('form').style.display = "block";
-}
-
-let itemsArray =[];
-let form = document.querySelector('.formBacklog');
+let form = document.querySelector('.formBacklog')
 let ul = document.querySelector('.ulBacklog');
 let button = document.querySelector('.clearAll');
 let input = document.querySelector('.nameTaskBacklog');
+let itemsArray =localStorage.getItem('itemsBacklog') ? JSON.parse(localStorage.getItem('itemsBacklog')) : []
 let data = JSON.parse(localStorage.getItem('itemsBacklog'))
 
-let liMaker = text => {	
+function liMaker(text) {
 let li = document.createElement('li');
 li.style.cssText = 'background: #ffffff, border-radius: 5px, padding: 7px, margin-bottom: 15px, cursor: pointer;';
 li.textContent = text;
@@ -20,32 +13,24 @@ li.classList.add('item');
 ul.append(li);
 }
 
+let btnAdd = document.querySelector('.addCardBacklog');
+btnAdd.addEventListener('click', showForm);
+function showForm(event) {
+	form.style.display = "block";
+}
+
 let btnSubmit = document.querySelector('.btnSubmit');
 btnSubmit.addEventListener('click', submit);
-
-function update () {
- itemsArray = localStorage.getItem('itemsBacklog') ? JSON.parse(localStorage.getItem('itemsBacklog')) : []
-}
-	
 function submit() {
 	itemsArray.push(input.value)
   localStorage.setItem('itemsBacklog', JSON.stringify(itemsArray))
   liMaker(input.value);
   input.value = '';
-	document.querySelector('form').style.display = "none";
+	form.style.display = "none";
 }
-window.onload = function() {
-	if (data.length > 0) {
-		update();
-		data.forEach(item => {
-		liMaker(item)
-		})
-	}
-};
-	
 button.addEventListener('click', function() {
   localStorage.clear();
-	update();
+	itemsArray = [];
   while (ul.firstChild) {
     ul.removeChild(ul.firstChild)
   }
